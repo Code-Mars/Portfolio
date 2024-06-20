77e88abd811bbfa9f096fcc27e42f798b3dc2d2d
Chandrabhan Maurya <chandra82001@gmail.com>
2024-06-21 00:27:30 +0530

    Implemented Header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,24 @@
 import { IconHexagonLetterC } from "@tabler/icons-react";
 import SideBar from "./SideBar";
 
+const links=["About","Work","Experience","Skills","Contact"];
+const navLinks=(col:Boolean)=>{
+    return links.map((link, index)=>{
+        return  <a  className={`${col?'flex flex-col items-center':''} text-textColor text-lg font-mono hover:text-primaryColor`} href={`#${link}`}><span className="text-primaryColor">0{index+1}. </span>{link}</a>
+    })
+}
 
 const Header=()=>{
 return (
-    <nav className="flex bg-bgColor h-[18vh] px-10 dm-mono-regular  justify-between items-center">
+    <nav className="flex bg-bgColor h-[18vh] px-10  justify-between items-center">
         
-        <IconHexagonLetterC size={60} color="#64FFDA" stroke={1.25}/>
+        <IconHexagonLetterC className="z-10" size={60} color="#64FFDA" stroke={1.25}/>
         <div className="md:flex gap-8 hidden">
-            <a  className="text-textColor hover:text-primaryColor" href="#about" >About</a>
-            <a  className="text-textColor hover:text-primaryColor" href="#work" >Work</a>
-            <a  className="text-textColor hover:text-primaryColor" href="#experience" >Experience</a>
-            <a  className="text-textColor hover:text-primaryColor" href="#skills" >Skills</a>
-            <a  className="text-textColor hover:text-primaryColor" href="#contact" >Contact</a>
+            {navLinks(false)}
         </div>
         <SideBar/>
     </nav>
 );
 }
 export default Header;
+export {navLinks};
--- src/Components/SideBar.tsx
+++ src/Components/SideBar.tsx
@@ -1,28 +1,23 @@
 import { useDisclosure } from '@mantine/hooks';
-import { Drawer, Button } from '@mantine/core';
+import { Drawer, Button, Burger } from '@mantine/core';
 import { IconHexagonLetterC, IconX } from '@tabler/icons-react';
+import { navLinks } from './Header';
 
 const SideBar=()=> {
-  const [opened, { open, close }] = useDisclosure(false);
+  const [opened, { toggle }] = useDisclosure(false);
 
   return (
     <>
-
-      
-      <Drawer.Root className='md:hidden'  position='right' opened={opened}   onClose={close} size="50vw" >
-        <Drawer.Overlay style={{ backgroundOpacity: 0.5, blur: 4 }} />
-        <Drawer.Content bg="#0A192F">
-          <Drawer.Header bg="#0A192F">
-            <Drawer.Title ><IconHexagonLetterC size={48} color="#64FFDA" stroke={1.25}/></Drawer.Title>
-            <Drawer.CloseButton className='hover:!bg-bgColor hover:!text-white'
-          icon= <IconX className='hover:text-primaryColor' size={30} stroke={2} />/>
-            
-          </Drawer.Header>
-          <Drawer.Body bg="#0A192F">Drawer content</Drawer.Body>
+      <Drawer.Root  className='md:hidden !-z-10'  position='right' opened={opened}   onClose={toggle} size="50vw" >
+        <Drawer.Overlay className='!-z-0' style={{ backgroundOpacity: 0.5, blur: 4 }} />
+        <Drawer.Content className='!-z-0' bg="#0A192F">
+          <Drawer.Body className='mt-20 flex flex-col gap-5' bg="#0A192F">
+            {navLinks(true)}
+          </Drawer.Body>
         </Drawer.Content>
       </Drawer.Root>
 
-      <Button className='md:!hidden' onClick={open}>Open Drawer</Button>
+      <Burger className='md:!hidden !z-10' size="lg" color='#64FFDA' opened={opened} onClick={toggle}  />
     </>
   );
 }
