79876dbfeefd2ea330bd364ad4ab1b7f07955378
Chandrabhan Maurya <chandra82001@gmail.com>
2024-07-05 01:22:23 +0530

    CMore Changes

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,7 +1,6 @@
 import About from "./About";
 import Contact from "./Contact";
 import Experience from "./Experience";
-import Footer from "./Footer";
 import Header from "./Header"
 import Mail from "./Mail";
 import Projects from "./Projects";
@@ -16,7 +15,6 @@ const HomePage = () => {
         <Skills />
         <Experience />
         <Contact />
-        <Footer />
         <Mail />
         <Social />
     </div>
