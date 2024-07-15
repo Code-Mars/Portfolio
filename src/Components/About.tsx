e2fc8868b1786273bbd752124dcb9662ae157feb
Chandrabhan Maurya <chandra82001@gmail.com>
2024-07-16 02:16:29 +0530

    Changes for deployment

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -34,7 +34,7 @@ const About = () => {
                     <div className="text-textColor text-xl w-[90%] text-justify my-8 lg-mx:my-0 font-semibold lg-mx:text-base sm-mx:text-sm xs-mx:text-xs">{Info.bio}</div>
                     <div className="xs-mx:w-[90%] flex gap-3 xs-mx:justify-between">
                         <Button onClick={open} className="focus-visible:!outline-none !text-bgColor !w-fit xs-mx:!w-[46%]" size={btn} variant="filled" color="#64FFDA">Check Resume</Button>
-                        <Button component="a" href="Marshal Resume.pdf" download={Info.name} className="focus-visible:!outline-none !text-primaryColor !w-fit xs-mx:!w-[46%]" size={btn} variant="outline" color="#64FFDA" rightSection={<IconDownload size={20} />}>Download</Button>
+                        <Button component="a" href="Resume.pdf" download={Info.name} className="focus-visible:!outline-none !text-primaryColor !w-fit xs-mx:!w-[46%]" size={btn} variant="outline" color="#64FFDA" rightSection={<IconDownload size={20} />}>Download</Button>
                     </div>
                 </div>
                 <div className="h-fit flex justify-center items-center rounded-full bs:mr-10 w-fit">
--- src/Components/ResumeViewer.tsx
+++ src/Components/ResumeViewer.tsx
@@ -15,7 +15,7 @@ const ResumeViewer = (props: any) => {
             <Modal.Header className="!bg-bgColor xs-mx:!p-2 !border-primaryColor xs-mx:!border  !border-2 xs-mx:!border-b-0 !border-b-0 !rounded-tl-3xl !rounded-tr-3xl">
                 <Modal.Title data-autofocus className="!text-4xl xs-mx:!text-2xl text-white flex gap-3 items-center !font-bold">Resume
                 <Tooltip label="Download" className="!text-bgColor" color="#64FFDA" position="right" offset={5}>
-                    <ActionIcon className="!text-primaryColor" component="a" href="Marshal Resume.pdf" size={btn} download={Info.name} variant="outline" color="#64FFDA">
+                    <ActionIcon className="!text-primaryColor" component="a" href="Resume.pdf" size={btn} download={Info.name} variant="outline" color="#64FFDA">
                         <IconArrowBigDownLineFilled className=" xs-mx:!w-[16px] xs-mx:!h-" />
                     </ActionIcon>
                     </Tooltip>
@@ -23,7 +23,7 @@ const ResumeViewer = (props: any) => {
                 <Modal.CloseButton size="md" iconSize="30px" className="!bg-bgColor !text-red-500" />
             </Modal.Header>
             <Modal.Body className="!bg-bgColor xs-mx:!p-2 !pt-2 !border-primaryColor  !border-2 xs-mx:!border xs-mx:!border-t-0 !border-t-0 !rounded-bl-3xl !rounded-br-3xl ">
-                <Document className="w-full !rounded-2xl !overflow-hidden !min-w-40 !min-h-14" file="Marshal Resume.pdf" >
+                <Document className="w-full !rounded-2xl !overflow-hidden !min-w-40 !min-h-14" file="Resume.pdf" >
                     <Page className="w-full !min-w-40 !min-h-14 md-mx:[&>.react-pdf\_\_Page\_\_canvas]:!w-full md-mx:[&>.react-pdf\_\_Page\_\_canvas]:!h-auto" pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
                 </Document>
             </Modal.Body>
